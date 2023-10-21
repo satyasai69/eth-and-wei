3682917c84f442cb59cdf324d4f708567779cc1b
satyasai69 <ch.satya.sai133@gmail.com>
2023-10-22 00:08:07 +0530

    ownership

--- Eth-and-wei.sol
+++ Eth-and-wei.sol
@@ -2,6 +2,18 @@
 pragma solidity ^0.8.19;
 
 contract EtherUnits {
+    
+   address _owner;
+    constructor () {
+        _owner = msg.sender;
+    }
+    modifier onlyOwner() {
+       require(_owner == msg.sender);
+       _;
+    }
+    function transferOwnerShip(address _newOwner) public onlyOwner{
+       _owner = _newOwner;
+    }
     uint public oneWei = 1 wei;
     // 1 wei is equal to 1
     bool public isOneWei = 1 wei == 1;
